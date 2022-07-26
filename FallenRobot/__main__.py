ee1d027be95b6fa8b4b311540b84c91c7652a3a2
allu <62532710+allu725@users.noreply.github.com>
2022-07-26 22:11:54 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -228,7 +228,7 @@ def start(update: Update, context: CallbackContext):
         else:
             first_name = update.effective_user.first_name
             update.effective_message.reply_sticker(
-                "CAACAgUAAxkBAAJYsmLWRvm70cE-mmxSNCovEf4v1ueJAAIcCAACbMK4VuL4EmZEkq8WKQQ"
+                "CAACAgUAAxkBAAJc-GIfWR3hIR_5MRjwkOdkT41wrBcHAAIVAQACyJRkFGZEMKKnFWwTHgQ"
             )
             update.effective_message.reply_text(
                 PM_START_TEXT.format(
